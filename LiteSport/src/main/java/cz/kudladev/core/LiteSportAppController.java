24c485e20b469f529e2898725c3d9b171f296e61
Kudl1k <stepanek.kudlacek@gmail.com>
2024-11-15 13:17:37 +0100

    Working log in and log out

--- LiteSport/src/main/java/cz/kudladev/core/LiteSportAppController.java
+++ LiteSport/src/main/java/cz/kudladev/core/LiteSportAppController.java
@@ -5,6 +5,7 @@ import cz.kudladev.LiteSportApp;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
 import java.net.URL;
@@ -20,10 +21,35 @@ public class LiteSportAppController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         userService = new UserService();
-        if (LiteSportAppState.getInstance().isUserLoggedIn()) {
-            System.out.println("User is logged in");
-        } else {
-            System.out.println("User is not logged in");
+        LiteSportAppState appState = LiteSportAppState.getInstance();
+
+        appState.getIsUserLoggedIn().addListener((observable, oldValue, newValue) -> {
+            LiteSportHeader.getChildren().clear(); // Clear existing children
+            if (newValue) {
+                System.out.println("User is logged in");
+                Button logoutButton = new Button("Logout");
+                logoutButton.setId("logoutButton");
+                logoutButton.setOnAction(e -> {
+                    //userService.logout();
+                    appState.setIsUserLoggedIn(false);
+                });
+                Label usernameLabel = new Label(appState.getLoggedInUser().getName() + " " + appState.getLoggedInUser().getSurname());
+                LiteSportHeader.getChildren().addAll(usernameLabel, logoutButton);
+            } else {
+                System.out.println("User is not logged in");
+                Button loginButton = new Button("Login");
+                loginButton.setId("loginButton");
+                loginButton.setOnAction(e -> LiteSportApp.openWindow("Login", 400, 400));
+                Button registerButton = new Button("Register");
+                registerButton.setId("registerButton");
+                registerButton.setOnAction(e -> LiteSportApp.openWindow("Register", 400, 400));
+                LiteSportHeader.getChildren().addAll(loginButton, registerButton);
+            }
+        });
+
+        // Initial check
+        if (!appState.getIsUserLoggedIn().getValue()) {
+            LiteSportHeader.getChildren().clear();
             Button loginButton = new Button("Login");
             loginButton.setId("loginButton");
             loginButton.setOnAction(e -> LiteSportApp.openWindow("Login", 400, 400));
--- LiteSport/src/main/java/cz/kudladev/core/LiteSportAppState.java
+++ LiteSport/src/main/java/cz/kudladev/core/LiteSportAppState.java
@@ -1,6 +1,8 @@
 package cz.kudladev.core;
 
 import DomainModels.UserDomainModel;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,11 +10,21 @@ import lombok.Setter;
 @Setter
 public class LiteSportAppState {
     private static LiteSportAppState instance = new LiteSportAppState();
-    private boolean isUserLoggedIn = false;
+    private BooleanProperty isUserLoggedIn = new SimpleBooleanProperty(false);
     private UserDomainModel loggedInUser = null;
 
     private LiteSportAppState() {
         // private constructor to prevent instantiation
+    }
+
+    public void setIsUserLoggedIn(boolean value) {
+        isUserLoggedIn.set(value);
+    }
+
+    public void setLoggedInUser(UserDomainModel user) {
+        System.out.println("User set");
+        loggedInUser = user;
+        isUserLoggedIn.set(true);
     }
 
     public static LiteSportAppState getInstance() {
